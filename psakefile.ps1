249858c04395916ffa731549233dd8deb1dec734
Jeffrey Patton <jeffrey@patton-tech.com>
2023-01-11 09:59:30 -0600

    scriptanalyzer was getting hun up on psake Task function

--- psakefile.ps1
+++ psakefile.ps1
@@ -27,46 +27,53 @@ Task LocalUse -description "Use for local testing" -depends Clean, BuildModule, 
 Task CIUSe -description "Use for pipeline deployments" -depends BuildModule, BuildManifest
 
 Task Clean {
-  $null = Remove-Item $Output -Recurse -ErrorAction Ignore
-  $null = New-Item -Type Directory -Path $Destination
+ $null = Remove-Item $Output -Recurse -ErrorAction Ignore
+ $null = New-Item -Type Directory -Path $Destination
 }
 
 Task BuildModule -description "Compile the Build Module" -action {
-  [System.Text.StringBuilder]$stringbuilder = [System.Text.StringBuilder]::new()
-  foreach ($folder in $imports ) {
-    [void]$stringbuilder.AppendLine( "Write-Verbose 'Importing from [$Source\$folder]'" )
-    if (Test-Path "$source\$folder") {
-      $fileList = Get-ChildItem "$source\$folder\*.ps1" -Exclude "*.Tests.ps1"
-      foreach ($file in $fileList) {
-        $shortName = $file.BaseName
-        Write-Output "  Importing [.$shortName]"
-        [void]$stringbuilder.AppendLine( "# .$shortName" )
-        [void]$stringbuilder.AppendLine( [System.IO.File]::ReadAllText($file.fullname) )
-      }
-    }
+ [System.Text.StringBuilder]$stringbuilder = [System.Text.StringBuilder]::new()
+ foreach ($folder in $imports )
+ {
+  [void]$stringbuilder.AppendLine( "Write-Verbose 'Importing from [$Source\$folder]'" )
+  if (Test-Path "$source\$folder")
+  {
+   $fileList = Get-ChildItem "$source\$folder\*.ps1" -Exclude "*.Tests.ps1"
+   foreach ($file in $fileList)
+   {
+    $shortName = $file.BaseName
+    Write-Output "  Importing [.$shortName]"
+    [void]$stringbuilder.AppendLine( "# .$shortName" )
+    [void]$stringbuilder.AppendLine( [System.IO.File]::ReadAllText($file.fullname) )
+   }
   }
-  Write-Output "  Creating module [$ModulePath]"
-  Set-Content -Path  $ModulePath -Value $stringbuilder.ToString()
+ }
+ Write-Output "  Creating module [$ModulePath]"
+ Set-Content -Path  $ModulePath -Value $stringbuilder.ToString()
 }
 
 Task BuildManifest -description "Compile the Module Manifest" -depends BuildModule -action {
-  Write-Output "  Update [$ManifestPath]"
-  Copy-Item "$Source\$ModuleName.psd1" -Destination $ManifestPath
-  $Functions = @()
-  foreach ($Folder in $PublicFunctions) {
-    if (Test-Path "$Source\$Folder") {
-      $FileList = Get-ChildItem "$($Source)\$($Folder.Replace('public',''))*.psd1"
-      foreach ($File in $FileList) {
-        $Functions += Get-Metadata -Path $File.FullName -PropertyName FunctionsToExport
-      }
-    }
+ Write-Output "  Update [$ManifestPath]"
+ Copy-Item "$Source\$ModuleName.psd1" -Destination $ManifestPath
+ $Functions = @()
+ foreach ($Folder in $PublicFunctions)
+ {
+  if (Test-Path "$Source\$Folder")
+  {
+   $FileList = Get-ChildItem "$($Source)\$($Folder.Replace('public',''))*.psd1"
+   foreach ($File in $FileList)
+   {
+    $Functions += Get-Metadata -Path $File.FullName -PropertyName FunctionsToExport
+   }
   }
-  Update-Metadata -Path $ManifestPath -PropertyName FunctionsToExport -Value $Functions
+ }
+ Update-Metadata -Path $ManifestPath -PropertyName FunctionsToExport -Value $Functions
 }
 
 Task PesterTest -description "Test module" -action {
-  $TestResults = Invoke-Pester -OutputFormat NUnitXml -OutputFile $TestFile
-  if ($TestResults.FailedCount -gt 0) {
-    Write-Error "Failed [$($TestResults.FailedCount)] Pester tests"
-  }
+ $TestResults = Invoke-Pester -OutputFormat NUnitXml -OutputFile $TestFile
+ if ($TestResults.FailedCount -gt 0)
+ {
+  Write-Error "Failed [$($TestResults.FailedCount)] Pester tests"
+ }
 }
