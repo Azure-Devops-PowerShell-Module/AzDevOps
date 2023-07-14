3ca5af519cf4c4af91466be4727ebdda57ae6ede
Jeffrey Patton <jeffrey@patton-tech.com>
2023-07-14 09:32:08 -0500

    issue-37: Feeds have a different root url structure

--- AzDevOps/azdevops/public/ConnectOrganization.ps1
+++ AzDevOps/azdevops/public/ConnectOrganization.ps1
@@ -10,20 +10,21 @@ function Connect-Organization
   [Parameter(Mandatory = $true)]
   [string]$PAT,
   [Parameter(Mandatory = $false)]
-  [ValidateSet('5.1', '7.1-preview.4')]
+  [ValidateSet('5.1', '7.1-preview.4','7.1-preview.1')]
   [string]$ApiVersion = '7.1-preview.4'
  )
  begin
  {
   Write-Verbose "ConnectOrganization : Begin Processing";
   Write-Verbose " Orgname            : $($Orgname)";
-  Write-Verbose " ApieVersion        : $($ApiVersion)";
+  Write-Verbose " ApiVersion         : $($ApiVersion)";
   try
   {
    $ErrorActionPreference = 'Stop';
    $Error.Clear();
    $azDevOpsHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAT)")) };
    $azDevOpsOrg = "https://dev.azure.com/$($Orgname)/";
+   $azDevOpsFeed = "https://feeds.dev.azure.com/$($Orgname)";
 
    $uriProjects = $azDevOpsOrg + "_apis/projects?api-version=$($ApiVersion)";
    $Result = Invoke-AdoEndpoint -Uri ([System.Uri]::new($uriProjects)) -Method get -Headers $azDevOpsHeader -Verbose:$VerbosePreference;
@@ -32,7 +33,12 @@ function Connect-Organization
     Set-Variable -Name azDevOpsHeader -Value $azDevOpsHeader -Scope Global;
     Set-Variable -Name azDevOpsOrg -Value $azDevOpsOrg -Scope Global;
     Set-Variable -Name azDevOpsConnected -Value $true -Scope Global;
-    return "Connected to $($azDevOpsOrg)"
+    Set-Variable -Name azDevOpsFeed -Value $azDevOpsFeed -Scope Global;
+    Write-Verbose " azDevpOpsHeader    : $($azDevOpsHeader)";
+    Write-Verbose " azDevpOpsOrg       : $($azDevOpsOrg)";
+    Write-Verbose " azDevpOpsConnected : $($azDevOpsConnected)";
+    Write-Verbose " azDevpOpsFeed      : $($azDevOpsFeed)";
+    return "Connected to $($azDevOpsOrg)";
    }
   }
   catch
