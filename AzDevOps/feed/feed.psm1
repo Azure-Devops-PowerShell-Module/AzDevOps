3f8a5d45b9f85efc916af95f2b8acb40987f22cd
Jeffrey Patton <jeffrey@patton-tech.com>
2023-07-14 09:33:24 -0500

    issue-37: Renamed folder structure slightly

---
+++ AzDevOps/feed/feed.psm1
@@ -0,0 +1,20 @@
+# .ExternalHelp feed-help.xml
+[cmdletbinding()]
+param()
+Write-Verbose "This psm1 is replaced in the build output. This file is only used for debugging."
+Write-Verbose $PSScriptRoot
+
+Write-Verbose 'Import everything in sub folders'
+foreach ($folder in @('classes', 'private', 'public', 'includes', 'internal'))
+{
+ $root = Join-Path -Path $PSScriptRoot -ChildPath $folder
+ if (Test-Path -Path $root)
+ {
+  Write-Verbose "processing folder $root"
+  $files = Get-ChildItem -Path $root -Filter *.ps1
+
+  # dot source each file
+  $files | where-Object { $_.name -NotLike '*.Tests.ps1' } |
+  ForEach-Object { Write-Verbose $_.basename; . $_.FullName }
+ }
+}
