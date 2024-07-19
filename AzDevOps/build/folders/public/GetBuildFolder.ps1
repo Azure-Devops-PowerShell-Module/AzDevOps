f41ba591e6923ba66b2f29bb77c2cb32e68793c9
Jeff Patton <jeffrey@patton-tech.com>
2024-07-19 16:24:32 -0500

    Removed recursion

--- AzDevOps/build/folders/public/GetBuildFolder.ps1
+++ AzDevOps/build/folders/public/GetBuildFolder.ps1
@@ -2,57 +2,74 @@ function Get-BuildFolder
 {
  [CmdletBinding(
   HelpURI = 'https://github.com/Azure-Devops-PowerShell-Module/build/blob/master/docs/Get-AdoBuildFolder.md#get-adobuildfolder',
-  PositionalBinding = $true)]
+  PositionalBinding = $true
+ )]
  [OutputType([Object])]
  param (
   [Parameter(ValueFromPipeline, Mandatory = $false, ParameterSetName = 'Project')]
   [object]$Project,
+
   [Parameter(Mandatory = $false, ParameterSetName = 'ProjectId')]
   [Guid]$ProjectId,
+
   [Parameter(Mandatory = $false, ParameterSetName = 'Project')]
   [Parameter(Mandatory = $false, ParameterSetName = 'ProjectId')]
   [string]$Path,
+
   [Parameter(Mandatory = $false)]
-  [ValidateSet('5.1-preview.2', '7.1-preview.2')]
-  [string]$ApiVersion = '7.1-preview.2'
+  [ValidateSet('5.1-preview.2', '7.1-preview.2','7.2-preview.2')]
+  [string]$ApiVersion = '7.2-preview.2'
  )
+
  process
  {
-  Write-Verbose "GetBuildFolder : Process Record";
+  Write-Verbose "Get-BuildFolder: Process Record"
   if ($PSCmdlet.ParameterSetName -eq 'Project')
   {
-   Write-Verbose " ProjectId     : $($Project.Id)";
+   Write-Verbose "Project URL: $($Project.url)"
   }
   else
   {
-   Write-Verbose " ProjectId     : $($ProjectId)";
+   Write-Verbose "ProjectId: $($ProjectId)"
   }
-  Write-Verbose " Path          : $($Path)";
-  Write-Verbose " ApiVersion    : $($ApiVersion)";
+  Write-Verbose "Path: $($Path)"
+  Write-Verbose "ApiVersion: $($ApiVersion)"
+
   try
   {
-   $ErrorActionPreference = 'Stop';
-   $Error.Clear();
-   #
-   # Are we connected
-   #
-   if ($Global:azDevOpsConnected)
+   $ErrorActionPreference = 'Stop'
+   $Error.Clear()
+
+   if (-not $Global:azDevOpsConnected)
    {
-    if ($PSCmdlet.ParameterSetName -eq 'ProjectId')
-    {
-     $Project = Get-AdoProject -ProjectId $ProjectId -Verbose:$VerbosePreference;
-    }
-    $Uri = $Global:azDevOpsOrg + "$($Project.Id)/_apis/build/folders?api-version=$($ApiVersion)";
-    if ($Path)
-    {
-     $Uri = $Uri + "&path=$($Path.Replace('\','/'))"
-    }
-    return (Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method Get -Headers $Global:azDevOpsHeader -Verbose:$VerbosePreference).Value;
+    throw "Not connected to Azure DevOps. Please connect using Connect-AzDevOps."
    }
+
+   $BaseUri = if ($PSCmdlet.ParameterSetName -eq 'Project')
+   {
+    "$($Global:azDevOpsOrg)$($Project.Id)/_apis/build/folders"
+   }
+   else
+   {
+    "$($Global:azDevOpsOrg)$($ProjectId)/_apis/build/folders"
+   }
+
+   $Uri = "$($BaseUri)?api-version=$($ApiVersion)"
+   if ($Path)
+   {
+    $Uri += "&path=$($Path.Replace('\', '/'))"
+   }
+
+   Write-Verbose "BaseUri: $($BaseUri)"
+   Write-Verbose "Uri: $($Uri)"
+
+   $Response = Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method Get -Headers $Global:azDevOpsHeader -Verbose:$VerbosePreference
+
+   return $Response.Value
   }
   catch
   {
-   throw $_;
+   throw $_
   }
  }
 }
