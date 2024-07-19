74fd894331e821f3f20f2bbb2283643fc18fb51f
Jeff Patton <jeffrey@patton-tech.com>
2024-07-19 16:37:44 -0500

    Removed recursion

--- AzDevOps/core/projects/public/GetProject.ps1
+++ AzDevOps/core/projects/public/GetProject.ps1
@@ -2,44 +2,63 @@ function Get-Project
 {
  [CmdletBinding(
   HelpURI = 'https://github.com/Azure-Devops-PowerShell-Module/projects/blob/master/docs/Get-AdoProject.md#get-adoproject',
-  PositionalBinding = $true)]
+  PositionalBinding = $true
+ )]
  [OutputType([Object])]
  param (
   [Parameter(Mandatory = $false)]
   [Guid]$ProjectId,
+
   [Parameter(Mandatory = $false)]
-  [ValidateSet('5.1', '7.1-preview.4')]
-  [string]$ApiVersion = '7.1-preview.4'
+  [ValidateSet('5.1', '7.1-preview.4','7.2-preview.4')]
+  [string]$ApiVersion = '7.2-preview.4'
  )
+
  begin
  {
-  Write-Verbose "GetProject    : Begin Processing";
-  Write-Verbose " ProjectId    : $($ProjectId)";
-  Write-Verbose " ApiVersion   : $($ApiVersion)";
+  Write-Verbose "GetProject: Begin Processing"
+  Write-Verbose "ProjectId: $($ProjectId)"
+  Write-Verbose "ApiVersion: $($ApiVersion)"
+ }
+
+ process
+ {
   try
   {
-   $ErrorActionPreference = 'Stop';
-   $Error.Clear();
-   #
-   # Are we connected
-   #
-   if ($Global:azDevOpsConnected)
+   $ErrorActionPreference = 'Stop'
+   $Error.Clear()
+
+   if (-not $Global:azDevOpsConnected)
    {
-    if ($ProjectId)
-    {
-     $Uri = $Global:azDevOpsOrg + "_apis/projects/$($ProjectId)?api-version=$($ApiVersion)";
-     return (Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method Get -Headers $Global:azDevOpsHeader -Verbose:$VerbosePreference);
-    }
-    else
-    {
-     $Uri = $Global:azDevOpsOrg + "_apis/projects?api-version=$($ApiVersion)";
-     return (Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method Get -Headers $Global:azDevOpsHeader -Verbose:$VerbosePreference).Value;
-    }
+    throw "Not connected to Azure DevOps. Please connect using Connect-AzDevOps."
+   }
+
+   if ($ProjectId)
+   {
+    $Uri = "$($Global:azDevOpsOrg)_apis/projects/$($ProjectId)?api-version=$($ApiVersion)"
+   }
+   else
+   {
+    $Uri = "$($Global:azDevOpsOrg)_apis/projects?api-version=$($ApiVersion)"
+   }
+
+   Write-Verbose "Uri: $($Uri)"
+
+   $Response = Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method Get -Headers $Global:azDevOpsHeader -Verbose:$VerbosePreference
+
+   if ($ProjectId)
+   {
+    return $Response
+   }
+   else
+   {
+    return $Response.Value
    }
   }
   catch
   {
-   throw $_;
+   Write-Error "Error retrieving project(s): $($_)"
+   throw $_
   }
  }
 }
