133328fbdda535a677d6b476ee6fd0db66a09a82
Jeff Patton <jeffrey@patton-tech.com>
2024-07-19 16:43:22 -0500

    removed recursion

--- AzDevOps/core/projects/public/GetProjectProperty.ps1
+++ AzDevOps/core/projects/public/GetProjectProperty.ps1
@@ -2,36 +2,47 @@ function Get-ProjectProperty
 {
  [CmdletBinding(
   HelpURI = 'https://github.com/Azure-Devops-PowerShell-Module/projects/blob/master/docs/Get-AdoProjectProperty.md#get-adoprojectproperty',
-  PositionalBinding = $true)]
+  PositionalBinding = $true
+ )]
  [OutputType([Object])]
  param (
   [Parameter(ValueFromPipeline)]
   [object]$Project,
+
   [Parameter(Mandatory = $false)]
-  [ValidateSet('5.1-preview.1', '7.1-preview.1')]
-  [string]$ApiVersion = '7.1-preview.1'
+  [ValidateSet('5.1-preview.1', '7.1-preview.1', '7.2-preview.1')]
+  [string]$ApiVersion = '7.2-preview.1'
  )
+
  begin
  {
-  Write-Verbose "GetBuild    : Begin Processing";
-  Write-Verbose " ProjectId  : $($Project.Id)";
-  Write-Verbose " ApiVersion : $($ApiVersion)";
+  Write-Verbose "GetBuild: Begin Processing"
+  Write-Verbose "ProjectId: $($Project.Id)"
+  Write-Verbose "ApiVersion: $($ApiVersion)"
+ }
+
+ process
+ {
   try
   {
-   $ErrorActionPreference = 'Stop';
-   $Error.Clear();
-   #
-   # Are we connected
-   #
-   if ($Global:azDevOpsConnected)
+   $ErrorActionPreference = 'Stop'
+   $Error.Clear()
+
+   if (-not $Global:azDevOpsConnected)
    {
-    $Uri = $Global:azDevOpsOrg + "_apis/projects/$($Project.id)/properties?api-version=$($ApiVersion)";
-    return (Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method Get -Headers $Global:azDevOpsHeader -Verbose:$VerbosePreference).Value;
+    throw "Not connected to Azure DevOps. Please connect using Connect-AzDevOps."
    }
+
+   $Uri = "$($Global:azDevOpsOrg)_apis/projects/$($Project.Id)/properties?api-version=$($ApiVersion)"
+   Write-Verbose "Uri: $($Uri)"
+
+   $Response = Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method Get -Headers $Global:azDevOpsHeader -Verbose:$VerbosePreference
+
+   return $Response.Value
   }
   catch
   {
-   throw $_;
+   throw $_
   }
  }
 }
--- AzDevOps/core/projects/public/NewProject.ps1
+++ AzDevOps/core/projects/public/NewProject.ps1
@@ -7,56 +7,68 @@ function New-Project
  param (
   [Parameter(Mandatory = $true)]
   [string]$Name,
+
   [Parameter(Mandatory = $false)]
   [string]$Description,
+
   [Parameter(Mandatory = $false)]
-  [ValidateSet('5.1', '7.1-preview.4')]
-  [string]$ApiVersion = '7.1-preview.4'
+  [ValidateSet('5.1', '7.1-preview.4', '7.2-preview.4')]
+  [string]$ApiVersion = '7.2-preview.4'
  )
+
  begin
  {
-  Write-Verbose "NewProject      : Begin Processing";
-  Write-Verbose " Name           : $($BuildId)";
-  Write-Verbose " Description    : $($BuildId)";
-  Write-Verbose " ApiVersion     : $($ApiVersion)";
+  Write-Verbose "NewProject: Begin Processing"
+  Write-Verbose "Name: $($Name)"
+  Write-Verbose "Description: $($Description)"
+  Write-Verbose "ApiVersion: $($ApiVersion)"
+ }
+
+ process
+ {
   try
   {
-   $ErrorActionPreference = 'Stop';
-   $Error.Clear();
-   #
-   # Are we connected
-   #
-   if ($Global:azDevOpsConnected)
+   $ErrorActionPreference = 'Stop'
+   $Error.Clear()
+
+   if (-not $Global:azDevOpsConnected)
    {
-    $Body = @{
-     "name"         = $Name
-     "description"  = $Description
-     "capabilities" = @{
-      "versioncontrol"  = @{
-       "sourceControlType" = "Git"
-      }
-      "processTemplate" = @{
-       "templateTypeId" = "b8a3a935-7e91-48b8-a94c-606d37c3e9f2"
-      }
-     }
-    } | ConvertTo-Json -Depth 5 -Compress;
-    $Uri = $Global:azDevOpsOrg + "_apis/projects?api-version=$($ApiVersion)";
-    if ($PSCmdlet.ShouldProcess("Create", "Create new project in $($Global:azDevOpsOrg) Azure Devops"))
-    {
-     $Result = Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method POST -Headers $Global:azDevOpsHeader -Body $Body -ContentType "application/json" -Verbose:$VerbosePreference;
-    }
-    do
-    {
-     $Status = Get-AdoOperations -OperationId $Result.id -Verbose:$VerbosePreference;
-     Write-Verbose $Status.status;
-    } until ($Status.status -eq 'succeeded')
-    Start-Sleep -Seconds 1;
-    Get-AdoProject  -Verbose:$VerbosePreference | Where-Object -Property name -eq $Name;
+    throw "Not connected to Azure DevOps. Please connect using Connect-AzDevOps."
    }
+
+   $Body = @{
+    "name"         = $Name
+    "description"  = $Description
+    "capabilities" = @{
+     "versioncontrol"  = @{
+      "sourceControlType" = "Git"
+     }
+     "processTemplate" = @{
+      "templateTypeId" = "b8a3a935-7e91-48b8-a94c-606d37c3e9f2"
+     }
+    }
+   } | ConvertTo-Json -Depth 5 -Compress
+
+   $Uri = "$($Global:azDevOpsOrg)_apis/projects?api-version=$($ApiVersion)"
+   Write-Verbose "Uri: $($Uri)"
+
+   if ($PSCmdlet.ShouldProcess("Create", "Create new project in $($Global:azDevOpsOrg) Azure DevOps"))
+   {
+    $Result = Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method POST -Headers $Global:azDevOpsHeader -Body $Body -ContentType "application/json" -Verbose:$VerbosePreference
+   }
+
+   do
+   {
+    $Status = Get-AdoOperations -OperationId $Result.id -Verbose:$VerbosePreference
+    Write-Verbose "Status: $($Status.status)"
+    Start-Sleep -Seconds 1
+   } until ($Status.status -eq 'succeeded')
+
+   Get-AdoProject -Verbose:$VerbosePreference | Where-Object -Property name -eq $Name
   }
   catch
   {
-   throw $_;
+   throw $_
   }
  }
 }
