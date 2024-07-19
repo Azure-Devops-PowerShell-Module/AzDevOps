934604250f73e10c80c4e286b95cf2848c0892ff
Jeff Patton <jeffrey@patton-tech.com>
2024-07-19 12:33:41 -0500

    This represents the changes that will be coming to all the functions as we work through this issue

--- AzDevOps/build/builds/public/GetBuild.ps1
+++ AzDevOps/build/builds/public/GetBuild.ps1
@@ -2,61 +2,94 @@ function Get-Build
 {
  [CmdletBinding(
   HelpURI = 'https://github.com/Azure-Devops-PowerShell-Module/build/blob/master/docs/Get-AdoBuild.md#get-adobuild',
-  PositionalBinding = $true)]
+  PositionalBinding = $true
+ )]
  [OutputType([Object])]
  param (
   [Parameter(ValueFromPipeline, Mandatory = $false, ParameterSetName = 'Project')]
   [object]$Project,
+
   [Parameter(Mandatory = $false, ParameterSetName = 'ProjectId')]
   [Guid]$ProjectId,
+
   [Parameter(Mandatory = $false, ParameterSetName = 'Project')]
   [Parameter(Mandatory = $false, ParameterSetName = 'ProjectId')]
   [int]$BuildId,
+
   [Parameter(Mandatory = $false)]
   [ValidateSet('5.1', '7.1-preview.7')]
   [string]$ApiVersion = '7.1-preview.7'
  )
+
  begin
  {
-  Write-Verbose "GetBuild    : Begin Processing";
+  Write-Verbose "Get-AdoBuild: Begin Processing"
   if ($PSCmdlet.ParameterSetName -eq 'Project')
   {
-   Write-Verbose " ProjectId  : $($Project.Id)";
+   Write-Verbose "Project URL: $($Project.url)"
   }
   else
   {
-   Write-Verbose " ProjectId  : $($ProjectId)";
+   Write-Verbose "ProjectId: $($ProjectId)"
   }
-  Write-Verbose " BuildId    : $($BuildId)";
-  Write-Verbose " ApiVersion : $($ApiVersion)";
+  Write-Verbose "BuildId: $($BuildId)"
+  Write-Verbose "ApiVersion: $($ApiVersion)"
+ }
+
+ process
+ {
   try
   {
-   $ErrorActionPreference = 'Stop';
-   $Error.Clear();
-   #
-   # Are we connected
-   #
-   if ($Global:azDevOpsConnected)
+   $ErrorActionPreference = 'Stop'
+   $Error.Clear()
+
+   if (-not $Global:azDevOpsConnected)
    {
-    if ($PSCmdlet.ParameterSetName -eq 'ProjectId')
-    {
-     $Project = Get-AdoProject -ProjectId $ProjectId -Verbose:$VerbosePreference;
-    }
-    $Uri = $Global:azDevOpsOrg + "$($Project.Id)/_apis/build/builds?api-version=$($ApiVersion)";
-    if ($BuildId)
-    {
-     $Uri = $Global:azDevOpsOrg + "$($Project.Id)/_apis/build/builds/$($BuildId)?api-version=$($ApiVersion)";
-     return (Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method Get -Headers $Global:azDevOpsHeader -Verbose:$VerbosePreference);
-    }
-    else
-    {
-     return (Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method Get -Headers $Global:azDevOpsHeader -Verbose:$VerbosePreference).Value;
-    }
+    throw "Not connected to Azure DevOps. Please connect using Connect-AzDevOps."
+   }
+
+   $BaseUri = if ($PSCmdlet.ParameterSetName -eq 'Project')
+   {
+    "$($Global:azDevOpsOrg)$($Project.Id)/_apis/build/builds"
+   }
+   else
+   {
+    "$($Global:azDevOpsOrg)$($ProjectId)/_apis/build/builds"
+   }
+
+   Write-Verbose "BaseUri: $BaseUri"
+
+   $Uri = if ($BuildId)
+   {
+    "$BaseUri/$BuildId?api-version=$ApiVersion"
+   }
+   else
+   {
+    "$BaseUri?api-version=$ApiVersion"
+   }
+
+   Write-Verbose "Uri: $Uri"
+
+   $Response = Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method Get -Headers $Global:azDevOpsHeader -Verbose:$VerbosePreference
+
+   if ($BuildId)
+   {
+    return $Response
+   }
+   else
+   {
+    return $Response.Value
    }
   }
   catch
   {
-   throw $_;
+   Write-Error "Error retrieving build(s): $_"
+   throw $_
   }
+ }
+
+ end
+ {
+  Write-Verbose "Get-AdoBuild: End Processing"
  }
 }
