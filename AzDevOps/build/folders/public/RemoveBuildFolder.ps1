551f09a4ef818f6a197c3ae321d1c73d64d9d49a
Jeff Patton <jeffrey@patton-tech.com>
2024-07-19 16:29:35 -0500

    Removed recursion

--- AzDevOps/build/folders/public/RemoveBuildFolder.ps1
+++ AzDevOps/build/folders/public/RemoveBuildFolder.ps1
@@ -7,53 +7,66 @@ function Remove-BuildFolder
  param (
   [Parameter(ValueFromPipeline, Mandatory = $false, ParameterSetName = 'Project')]
   [object]$Project,
+
   [Parameter(Mandatory = $false, ParameterSetName = 'ProjectId')]
   [Guid]$ProjectId,
+
   [Parameter(Mandatory = $true, ParameterSetName = 'Project')]
   [Parameter(Mandatory = $true, ParameterSetName = 'ProjectId')]
   [string]$Name,
+
   [Parameter(Mandatory = $false)]
-  [ValidateSet('5.0-preview.2', '7.1-preview.2')]
-  [string]$ApiVersion = '7.1-preview.2'
+  [ValidateSet('5.0-preview.2', '7.1-preview.2', '7.2-preview.2')]
+  [string]$ApiVersion = '7.2-preview.2'
  )
+
  process
  {
-  Write-Verbose "RemoveBuildFolder : Process Record";
+  Write-Verbose "RemoveBuildFolder: Process Record"
   if ($PSCmdlet.ParameterSetName -eq 'Project')
   {
-   Write-Verbose " ProjectId        : $($Project.Id)";
+   Write-Verbose "ProjectId: $($Project.Id)"
   }
   else
   {
-   Write-Verbose " ProjectId        : $($ProjectId)";
+   Write-Verbose "ProjectId: $($ProjectId)"
   }
-  Write-Verbose " Name             : $($Path)";
-  Write-Verbose " ApiVersion       : $($ApiVersion)";
+  Write-Verbose "Name: $($Name)"
+  Write-Verbose "ApiVersion: $($ApiVersion)"
+
   try
   {
-   $ErrorActionPreference = 'Stop';
-   $Error.Clear();
-   #
-   # Are we connected
-   #
-   if ($Global:azDevOpsConnected)
+   $ErrorActionPreference = 'Stop'
+   $Error.Clear()
+
+   if (-not $Global:azDevOpsConnected)
    {
-    if ($PSCmdlet.ParameterSetName -eq 'ProjectId')
-    {
-     $Project = Get-AdoProject -ProjectId $ProjectId -Verbose:$VerbosePreference;
-    }
-    $Folder = Get-AdoBuildFolder -Project $Project -Path $Name -Verbose:$VerbosePreference;
-    $Uri = $Global:azDevOpsOrg + "$($Project.Id)/_apis/build/folders/?api-version=$($ApiVersion)&path=$($Folder.Path)"
-    if ($PSCmdlet.ShouldProcess("Delete", "Remove Folder $($Name) from $($Project.name) Azure Devops Projects"))
-    {
-     Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method DELETE -Headers $Global:azDevOpsHeader -Verbose:$VerbosePreference;
-     $Project | Get-AdoBuildFolder -Verbose:$VerbosePreference;
-    }
+    throw "Not connected to Azure DevOps. Please connect using Connect-AzDevOps."
+   }
+
+   $BaseUri = if ($PSCmdlet.ParameterSetName -eq 'Project')
+   {
+    "$($Global:azDevOpsOrg)$($Project.Id)/_apis/build/folders"
+   }
+   else
+   {
+    "$($Global:azDevOpsOrg)$($ProjectId)/_apis/build/folders"
+   }
+
+   $Uri = "$($BaseUri)?api-version=$($ApiVersion)&path=$($Name.Replace('\', '/'))"
+
+   Write-Verbose "BaseUri: $($BaseUri)"
+   Write-Verbose "Uri: $($Uri)"
+
+   if ($PSCmdlet.ShouldProcess("Delete", "Remove Folder $($Name) from $($Project.name) Azure DevOps Projects"))
+   {
+    Invoke-AdoEndpoint -Uri ([System.Uri]::new($Uri)) -Method DELETE -Headers $Global:azDevOpsHeader -Verbose:$VerbosePreference
+    Write-Output "Folder $($Name) removed from project $($Project.name)"
    }
   }
   catch
   {
-   throw $_;
+   throw $_
   }
  }
 }
